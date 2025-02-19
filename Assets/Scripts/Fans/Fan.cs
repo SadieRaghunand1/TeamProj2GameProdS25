1b2a254e662c9e2413194a59993c3a3b0dd0f618
SadieRaghunand1 <sadielrowl@outlook.com>
2025-02-18 20:30:43 -0500

    Pickups, guard periphiary, restart level
    Pickups integrated with fan, guard has peripheral vision, if player falls off the ground the level restarts

--- Assets/Scripts/Fans/Fan.cs
+++ Assets/Scripts/Fans/Fan.cs
@@ -9,21 +9,25 @@ public class Fan : MonoBehaviour
     [SerializeField] private float fanForce;
     [SerializeField] private float minYBeforeForceAgain;
 
-   /* private void OnTriggerEnter(Collider other)
-    {
-        FanForceUp(other);
-    }*/
+    public bool needsKeys;
 
     private void OnTriggerStay(Collider other)
     {
         FanForceUp(other);
     }
 
-
+    /// <summary>
+    /// Generates force upwards of fan
+    /// </summary>
     void FanForceUp(Collider _other)
     {
-        if(_other.gameObject.GetComponent<PlayerMovement>() != null)
+        if(_other.gameObject.layer == 7)
         {
+            //Checks if this fan requires keys to operate, if it does and the number of keys isn't correct, exits function
+            if(needsKeys && _other.gameObject.GetComponent<Pickup>().CheckGoal() == false)
+            {
+                return;
+            }
             playerRB = _other.gameObject.GetComponent<Rigidbody>();
             playerRB.AddForce(Vector3.up * fanForce, ForceMode.Impulse);
 
@@ -32,5 +36,5 @@ public class Fan : MonoBehaviour
                 playerRB.AddForce(Vector3.up * fanForce, ForceMode.Impulse);
             }
         }
-    }
+    } //END FanForceUp()
 }
--- Assets/Scripts/Player/Pickup.cs
+++ Assets/Scripts/Player/Pickup.cs
@@ -9,16 +9,20 @@ public class Pickup : MonoBehaviour
     //Level 2
 
     //Attach to player
-    public int numCollected;
+    private int numCollected;
     [SerializeField] private TextMeshProUGUI numUI;
 
-    [SerializeField] private int goalCollected;
+    [SerializeField] int goalCollected;
+    private bool reachedGoal;
 
     private void OnCollisionEnter(Collision collision)
     {
         PickupObject(collision);
     }
 
+    /// <summary>
+    /// On collision with pickup, increases the number of pickups collected
+    /// </summary>
     void PickupObject(Collision _collision)
     {
         if(_collision.gameObject.layer == 9)
@@ -27,16 +31,24 @@ public class Pickup : MonoBehaviour
             Destroy(_collision.gameObject);
             numUI.text = "Collected: " + numCollected;
 
-            CheckGoal();
+            
         }
-    }
+    } //END PickupObject()
 
-    void CheckGoal()
+    /// <summary>
+    /// Checks if player has the required number of pickups when on fans that require pickups
+    /// </summary>
+    public bool CheckGoal()
     {
         if(numCollected == goalCollected)
         {
-            //Add end game functionality
+            reachedGoal = true;
+            
             Debug.Log("Goal collected");
         }
-    }
+
+        return reachedGoal;
+    } //END CheckGoal()
+
+    
 }
