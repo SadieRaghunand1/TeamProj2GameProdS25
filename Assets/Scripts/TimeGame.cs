0f7b32d3a26e6764148f4967174b3e8687e383ce
SadieRaghunand1 <sadielrowl@outlook.com>
2025-02-26 15:36:24 -0500

    Add ui changes for options i game scene

--- Assets/Scripts/TimeGame.cs
+++ Assets/Scripts/TimeGame.cs
@@ -15,7 +15,7 @@ public class TimeGame : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        InitValues();
     }
 
     // Update is called once per frame
